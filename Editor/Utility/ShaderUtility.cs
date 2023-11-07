980d11f714bb64d7b97a94be9116d80f22696cae
Jan Bureš <jan.bures@live.com>
2023-11-07 01:01:19 +0100

    Added a shader utility for fetching all shader parameter names

---
+++ Editor/Utility/ShaderUtility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace LFO.Editor.Utility
+{
+    public static class ShaderUtility
+    {
+        public static string[] GetShaderPropertyNames(Shader shader)
+        {
+            if (shader == null)
+            {
+                return Array.Empty<string>();
+            }
+
+            int propertyCount = ShaderUtil.GetPropertyCount(shader);
+            string[] propertyNames = new string[propertyCount];
+
+            for (int i = 0; i < propertyCount; i++)
+            {
+                string propertyName = ShaderUtil.GetPropertyName(shader, i);
+                propertyNames[i] = propertyName;
+            }
+
+            return propertyNames;
+        }
+    }
+}
+
